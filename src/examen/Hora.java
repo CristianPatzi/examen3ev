b297b232dadb429e99a2c3ee58704a10f8b494ee
CristianPatzi <crisfernandoxdpatzifrias@gmail.com>
2024-05-29 09:34:26 +0200

    proyectodoc

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,8 +1,18 @@
 package examen;
 
+/**
+ * @author Cristian
+ * @version 1.0
+ */
+
 public class Hora {
+	boolean horaValida;
+/**
+ * Metodo publico para validar una hora
+ * return boolean
+ */
 	public boolean validarHora(int segundos, int minutos, int horas) {
-		boolean horaValida = true;
+		horaValida = true;
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
 			if (horas > 23) {
 				horaValida = false;

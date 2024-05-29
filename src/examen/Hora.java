78bb130b78e9a21abd8f2fa792acce9891185501
CristianPatzi <crisfernandoxdpatzifrias@gmail.com>
2024-05-29 09:24:32 +0200

    proyectosindoc

---
+++ src/examen/Hora.java
@@ -0,0 +1,20 @@
+package examen;
+
+public class Hora {
+	public boolean validarHora(int segundos, int minutos, int horas) {
+		boolean horaValida = true;
+		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
+			if (horas > 23) {
+				horaValida = false;
+			} else if (minutos > 59) {
+				horaValida = false;
+			} else if (segundos > 59) {
+				horaValida = false;
+			}
+		} else {
+			horaValida = false;
+		}
+		return horaValida;
+	}
+}
+ 
